9be13122409b70ecfaaddddeea6c7c709986d334
Andrew-Wichmann <andrewwichmann@siu.edu>
2017-05-20 15:12:32 -0500

    seperate windows works

--- app.py
+++ app.py
@@ -4,17 +4,16 @@ import string
 import sys
 import parser
 
+colors=['red','blue','green','yellow','purple']
+figures=[]
+
 with open(sys.argv[1], 'r') as f:
     struct = parser.parse_file(f)
 
-plt.scatter(struct[1].mylist,struct[2].mylist, color="red", label=struct[1].title + " vs " + struct[2].title)
-plt.legend()
-plt.xlabel(struct[1].title)
-plt.ylabel(struct[2].title)
-
-plt.scatter(struct[0].mylist,struct[1].mylist, color="blue", label=struct[0].title + " vs " + struct[1].title)
-plt.legend()
-plt.xlabel(struct[0].title)
-plt.ylabel(struct[1].title)
-
-plt.show()
+for i in range(0, len(struct)-1):
+    fig = plt.figure(i)
+    plt.scatter(struct[i].mylist,struct[i+1].mylist, color=colors[i], label=struct[i].title + " vs " + struct[i+1].title)
+    plt.legend()
+    fig.show()
+    
+raw_input()
