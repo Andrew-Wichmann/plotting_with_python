d1d37e78cbc88fa18dc1c0e556d15284d842faa1
Andrew-Wichmann <andrewwichmann@siu.edu>
2017-05-21 20:24:55 -0500

    Now puts multiple figures

--- app.py
+++ app.py
@@ -10,9 +10,11 @@ with open(sys.argv[1], 'r') as f:
     struct = parser.parse_file(f)
 
 for i in range(1, len(struct)):
+    fig = plt.figure(i)
     plt.hist(struct[i].mylist, color=colors[i], label=struct[i].title)
     plt.legend()
-    plt.xlabel(struct[1].title)
-    plt.ylabel(struct[2].title)
+    plt.xlabel(struct[i].title)
+    plt.ylabel("frequency")
+    fig.show()
 
-plt.show()
+raw_input()
