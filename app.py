2955f927bfb76255d66c6d506bb2e95032b359e6
Andrew-Wichmann <andrewwichmann@siu.edu>
2017-05-21 20:21:17 -0500

    the histogram works

--- app.py
+++ app.py
@@ -4,13 +4,13 @@ import string
 import sys
 import parser
 
-colors=['green', 'purple', 'red','blue','yellow']
+colors=['green', 'purple', 'red',  'blue', 'yellow']
 
 with open(sys.argv[1], 'r') as f:
     struct = parser.parse_file(f)
 
-for i in range(0, len(struct)-1):
-    plt.scatter(struct[i].mylist,struct[i+1].mylist, color=colors[i], label=struct[i].title + " vs " + struct[i+1].title)
+for i in range(1, len(struct)):
+    plt.hist(struct[i].mylist, color=colors[i], label=struct[i].title)
     plt.legend()
     plt.xlabel(struct[1].title)
     plt.ylabel(struct[2].title)
