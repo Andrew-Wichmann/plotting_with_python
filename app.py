3a7effaeae4ba403621047969d6917b027dda203
Andrew-Wichmann <andrewwichmann@siu.edu>
2017-05-21 21:19:09 -0500

    now with fitted lines!

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import matplotlib.pyplot as plt
+import scipy.optimize as optimization
 import numpy as np
 import string
 import sys
@@ -10,10 +11,28 @@ figures=[]
 with open(sys.argv[1], 'r') as f:
     struct = parser.parse_file(f)
 
-for i in range(0, len(struct)-1):
+def plot_figure():
     fig = plt.figure(i)
-    plt.scatter(struct[i].mylist,struct[i+1].mylist, color=colors[i], label=struct[i].title + " vs " + struct[i+1].title)
+
+    reg_variable = struct[i].title
+    dep_variable = struct[i+1].title
+    x = np.array(struct[i].mylist)
+    y = np.array(struct[i+1].mylist)
+
+    plt.scatter(x, y, color=colors[i], label=reg_variable + " vs " + dep_variable)
+
+    m, c = find_least_squares(x, y)
+
+    plt.plot(x, m*x + c, 'r', label='Fitted line')
     plt.legend()
     fig.show()
+
+def find_least_squares(x, y):
+    A = np.vstack([x, np.ones(len(x))]).T
+    m, c = np.linalg.lstsq(A, y)[0]
+    return m, c
+
+for i in range(0, len(struct)-1):
+    plot_figure()
     
 raw_input()
